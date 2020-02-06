73f0676e761d2b4385c41b9faaaa6821cb4ba7c3
jhemphill <jhemphill@tecnicocorp.com>
2020-02-06 11:57:09 -0500

    Fixed output synchronicity, and changed to absolute file path mapping

--- secret.js
+++ secret.js
@@ -1,19 +1,18 @@
 'use strict';
 const fs = require('fs');
 const paths = require('path');
-const secretsDir = 'secure';
-fs.readdir(secretsDir, function (err, files) {
-    let len = files.length;
-    let packagedSecrets = {};
-    for (var i = 0; i < len; i++) {
-        if (files[i].substr(files[i].length-2,2) === 'js'){
-            let f = paths.join(secretsDir, files[i]);
-            let stat = fs.lstatSync(f);
-            if (stat.isFile() || stat.isSymbolicLink()){
-                let m = require(f);
-                Object.assign(packagedSecrets,m);
-            }
+const secretsDir = paths.join(__dirname,'secure');
+const files = fs.readdirSync(secretsDir);
+let len = files.length;
+let packagedSecrets = {};
+for (var i = 0; i < len; i++) {
+    if (files[i].substr(files[i].length-2,2) === 'js'){
+        let f = paths.join(secretsDir, files[i]);
+        let stat = fs.lstatSync(f);
+        if (stat.isFile() || stat.isSymbolicLink()){
+            let m = require(f);
+            Object.assign(packagedSecrets,m.default);
         }
     }
-    process.stdout.write(JSON.stringify(packagedSecrets));
-});
+}
+process.stdout.write(JSON.stringify(packagedSecrets));
